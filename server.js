af8cb5f92a821f31100dd048ae618b27ad55b490
iZafor <izafor65@gmail.com>
2022-12-13 20:12:30 +0600

    added multiple insertion support

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
-const uri = process.env.DB_URI || "mongodb://127.0.0.1:27017/noteDb";
+const uri = "mongodb://127.0.0.1:27017/noteDb";
 mongoose.connect(uri, (err) => {
     if (err) return console.log(err);
     console.log("Connected to the db succecssfuly");
@@ -24,54 +24,63 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.route("/")
-    .get((req, res) => {
-        Note.find({}, (err, foundNotes) => {
-            if (err) {
-                console.log(err);
-                return res.json({});
-            };
-            res.json(foundNotes);
+app.get("/", (req, res) => {
+    Note.find({}).then(foundNotes => res.json(foundNotes))
+        .catch(err => res.json({
+            message: err
+        }))
+})
+
+app.post("/add/one", (req, res) => {
+    const data = req.body;
+    const newNote = new Note({
+        __id: data.__id,
+        title: data.title,
+        content: data.content
+    });
+    newNote.save().then(() => res.json({
+        message: "transaction succesfull"
+    })).catch(err => res.json({
+        message: err.message
+    }));
+})
+
+app.post("/add/many", (req, res) => {
+    Note.insertMany(req.body).then(response => {
+        res.json({
+            message: "transaction successfull"
         })
-    }).post((req, res) => {
-        const data = req.body;
-        const newNote = new Note({
-            __id: data.__id,
-            title: data.title,
-            content: data.content
-        });
-        newNote.save().then(() => res.json({
-            message: "transaction succesfull"
-        })).catch(err => res.json({
-            message: err.message
-        }));
+    }).catch(err => {
+        res.json({
+            message: err
+        })
     })
+})
 
 app.delete("/delete/all", (req, res) => {
-    Note.deleteMany({}, (err, response) => {
-        if (err) {
-            return res.json({
-                message: err
-            })
-        }
+    Note.deleteMany({}).then(response => {
         res.json({
             message: response
+        })
+    }).catch(err => {
+        res.json({
+            message: err
         })
     })
 })
 
 app.delete("/delete/:__id", (req, res) => {
-    Note.deleteOne({ __id: req.params.__id }, (err, response) => {
-        if (err) {
-            return res.json({
-                message: err
-            })
-        }
+    Note.deleteOne({ __id: req.params.__id }).then(response => {
         res.json({
             message: response
         })
+    }).catch(err => {
+        res.json({
+            message: err
+        })
     })
 })
+
 
 
 app.listen(PORT, () => console.log(`app is connected to port ${PORT}`));
